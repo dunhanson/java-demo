cee50845017cd11a1166e72dbb66f2d7f2d9b2dc
dunhanson <dunhanson@gmail.com>
2022-08-20 18:45:06 +0800

    feat: AtomicExample1

---
+++ gradle/jdk8/concurrency-demo/src/main/java/site/dunhanson/concurrency/demo/basic/AtomicExample1.java
@@ -0,0 +1,25 @@
+package site.dunhanson.concurrency.demo.basic;
+
+public class AtomicExample1 {
+    volatile int i = 0;
+
+    public synchronized void incr() {
+        i++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicExample1 atomicExample = new AtomicExample1();
+        Thread[] threads = new Thread[2];
+        for(int i = 0; i < 2; i++) {
+            threads[i] = new Thread(()->{
+                for(int j = 0; j < 10000; j++) {
+                    atomicExample.incr();
+                }
+            });
+            threads[i].start();
+        }
+        threads[0].join();
+        threads[1].join();
+        System.out.println(atomicExample.i);
+    }
+}
