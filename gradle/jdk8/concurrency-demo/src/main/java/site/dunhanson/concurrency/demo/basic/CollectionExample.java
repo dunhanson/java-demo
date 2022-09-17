889fe83ca4fc60c5cd2f38b9edb9b4e38ba8705f
dunhanson <dunhanson@gmail.com>
2022-09-17 10:27:13 +0800

    feat: 集合示例CollectionExample

--- gradle/jdk8/concurrency-demo/src/main/java/site/dunhanson/concurrency/demo/basic/CollectionExample.java
+++ gradle/jdk8/concurrency-demo/src/main/java/site/dunhanson/concurrency/demo/basic/CollectionExample.java
@@ -15,7 +15,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 /**
@@ -25,7 +28,6 @@ import java.util.stream.Collectors;
 public class CollectionExample {
     Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
     Type type = new TypeToken<List<Employee>>(){}.getType();
-    DateTimeFormatter dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
 
     @Data
     static class Employee {
@@ -40,13 +42,22 @@ public class CollectionExample {
         }
     }
 
+    /**
+     * 获取数据
+     * @return List<Employee>
+     * @throws IOException IO异常
+     */
     public List<Employee> listData() throws IOException {
         String path = "D:\\test\\mysql\\employees.json";
         String json = FileUtils.readFileToString(new File(path), Charset.defaultCharset());
         return gson.fromJson(json, type);
     }
 
-    public void testDate() throws IOException {
+    /**
+     * 测试数据
+     * @throws IOException IO异常
+     */
+    public void testData() throws IOException {
         LocalDateTime start = LocalDateTime.now();
         CollectionExample example = new CollectionExample();
         List<Employee> employees = example.listData();
@@ -58,7 +69,11 @@ public class CollectionExample {
         log.info("testDate 耗时:{}ms", Duration.between(start, LocalDateTime.now()).toMillis());
     }
 
-    public void testDate1() throws IOException {
+    /**
+     * 测试数据1
+     * @throws IOException IO异常
+     */
+    public void testData1() throws IOException {
         LocalDateTime start = LocalDateTime.now();
         CollectionExample example = new CollectionExample();
         List<Employee> employees = example.listData();
@@ -70,9 +85,44 @@ public class CollectionExample {
         log.info("testDate1 耗时:{}ms", Duration.between(start, LocalDateTime.now()).toMillis());
     }
 
+    /**
+     * 测试数据用线程池
+     * @throws IOException IO异常
+     */
+    public void testDataWithExecutorService() throws IOException {
+        LocalDateTime start = LocalDateTime.now();
+        CollectionExample example = new CollectionExample();
+        // 线程池对象
+        ExecutorService service = Executors.newFixedThreadPool(12);
+        // Future集合
+        List<Future<LocalDate>> futureList = Collections.synchronizedList(new ArrayList<>());
+        // List<Employee>
+        List<Employee> employees = example.listData();
+        // 遍历
+        employees.forEach(employee -> {
+            Future<LocalDate> future = service.submit(() -> {
+                String birthDate = employee.getBirthDate();
+                return LocalDate.parse(birthDate, DateTimeFormatter.ISO_LOCAL_DATE);
+            });
+            futureList.add(future);
+        });
+        List<LocalDate> dateList = futureList.stream().map(future-> {
+            try {
+                return future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.toList());
+        log.info("testDataWithExecutorService size:{}", dateList.size());
+        log.info("testDataWithExecutorService 耗时:{}ms", Duration.between(start, LocalDateTime.now()).toMillis());
+        service.shutdown();
+    }
+
     public static void main(String[] args) throws IOException {
         CollectionExample example = new CollectionExample();
-        example.testDate();
-        example.testDate1();
+        // example.testData();
+        example.testData1();
+        example.testData1();
+        example.testDataWithExecutorService();
     }
 }
