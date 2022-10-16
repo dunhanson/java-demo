1d730e5f2166fed1425c1373fb4f0cec529e7dee
dunhanson <dunhanson@gmail.com>
2022-10-16 17:12:10 +0800

    feat: spring boot mq demo

---
+++ gradle/jdk8/spring-boot-mq-demo/src/test/java/site/dunhanson/spring/boot/mq/demo/SpringBootMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dunhanson.spring.boot.mq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
