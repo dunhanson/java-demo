4246b95ba58c16ce023fd1dfc20c255c5a1ab8bc
dunhanson <dunhanson@gmail.com>
2022-10-18 11:39:08 +0800

    feat: 添加类注释-1

--- maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/collection/queue/StackExample.java
+++ maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/collection/queue/StackExample.java
@@ -3,6 +3,11 @@ package site.dunhanson.basic.demo.collection.queue;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * 栈（Stack）是一种后进先出（LIFO：Last In First Out）的数据结构。
+ * @author dunhanson
+ * @since 2022-10-18
+ */
 public class StackExample {
     public static void main(String[] args) {
         Deque<String> deque = new LinkedList<>();
