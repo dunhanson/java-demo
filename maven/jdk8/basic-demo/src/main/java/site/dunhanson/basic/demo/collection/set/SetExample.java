e91b75e083291c7be5d63e6f2db15e02bfecd89b
dunhanson <dunhanson@gmail.com>
2022-10-19 18:40:50 +0800

    feat: SetExample

---
+++ maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/collection/set/SetExample.java
@@ -0,0 +1,24 @@
+package site.dunhanson.basic.demo.collection.set;
+
+public class SetExample {
+
+    public void testHashSet() {
+
+    }
+
+    public void testLinkedHashSet() {
+
+    }
+
+    public void testTreeSet() {
+
+    }
+
+    public void testEnumSet() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
