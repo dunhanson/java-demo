61db49ea9399174513d9d27a00889fe05ac320aa
dunhanson <dunhanson@gmail.com>
2022-10-30 15:08:53 +0800

    feat: javadoc-1

--- maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/concurrency/basic/TaskQueue.java
+++ maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/concurrency/basic/TaskQueue.java
@@ -8,22 +8,22 @@ import java.util.Queue;
 /**
  * 使用wait和notify
  *
- * <p>TaskQueue
- * <br>1、TaskQueue中包含一个queue集合属性，LinkedList实现类。
- * <br>2、两个方法：addTask、getTask
- * <br>3、addTask(String s)，synchronized修饰，添加完毕后notifyAll
- * <br>4、getTask()，synchronized修饰，内部做queue集合while为空判断，为空调用wait()方法，否则queue remove()后返回
+ * <p>TaskQueue<br>
+ * 1、TaskQueue中包含一个queue集合属性，LinkedList实现类。<br>
+ * 2、两个方法：addTask、getTask<br>
+ * 3、addTask(String s)，synchronized修饰，添加完毕后notifyAll<br>
+ * 4、getTask()，synchronized修饰，内部做queue集合while为空判断，为空调用wait()方法，否则queue remove()后返回
  *
- * <p>main()
- * <br>1、for循环创建5个线程，run方法内部进行执行while(true)，然后调用q.getTask()方法
- * <br>2、创建一个add线程，for循环10次，调用a.addTask(s)方法进行添加随机数
- * <br>3、add线程启动、add线程join，主线程睡眠100毫秒，for循环ts集合中的线程进行interrupt
+ * <p>main()<br>
+ * 1、for循环创建5个线程，run方法内部进行执行while(true)，然后调用q.getTask()方法<br>
+ * 2、创建一个add线程，for循环10次，调用a.addTask(s)方法进行添加随机数<br>
+ * 3、add线程启动、add线程join，主线程睡眠100毫秒，for循环ts集合中的线程进行interrupt
  *
- * <p>wait()
- * <br>调用wait()方法后，线程进入等待状态，wait()方法不会返回，直到将来某个时刻，线程从等待状态被其他线程唤醒后，wait()方法才会返回，然后，继续执行下一条语句。
+ * <p>wait()<br>
+ * 调用wait()方法后，线程进入等待状态，wait()方法不会返回，直到将来某个时刻，线程从等待状态被其他线程唤醒后，wait()方法才会返回，然后，继续执行下一条语句。
  *
- * <p>notify()
- * <br>会唤醒一个正在this锁等待的线程，从而使得等待线程从this.wait()方法返回。
+ * <p>notify()<br>
+ * 会唤醒一个正在this锁等待的线程，从而使得等待线程从this.wait()方法返回。
  *
  * @author dunhanson
  * @since 2022-10-30
