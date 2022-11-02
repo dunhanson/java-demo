4bc68891ee12b8bb22bfcf926e379a753e9deb02
dunhanson <dunhanson@gmail.com>
2022-11-02 21:52:54 +0800

    feat: DaemonThread1Example&DaemonThread2Example

---
+++ maven/jdk8/basic-demo/src/main/java/site/dunhanson/basic/demo/concurrency/basic/DaemonThread1Example.java
@@ -0,0 +1,11 @@
+package site.dunhanson.basic.demo.concurrency.basic;
+
+/**
+ * 守护线程
+ * <p>当 JVM 中不存在任何一个正在运行的非守护线程时，则 JVM 进程即会退出。<br>
+ * @author dunhanson
+ * @since 2022-11-02
+ */
+public class DaemonThread1Example {
+
+}
