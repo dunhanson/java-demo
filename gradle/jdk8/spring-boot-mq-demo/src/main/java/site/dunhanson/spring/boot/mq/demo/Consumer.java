936baab68173ec8c21142c06b5667efe1fb77a46
dunhanson <dunhanson@gmail.com>
2022-11-06 00:14:21 +0800

    feat: max.poll.interval.ms

--- gradle/jdk8/spring-boot-mq-demo/src/main/java/site/dunhanson/spring/boot/mq/demo/Consumer.java
+++ gradle/jdk8/spring-boot-mq-demo/src/main/java/site/dunhanson/spring/boot/mq/demo/Consumer.java
@@ -1,12 +1,18 @@
 package site.dunhanson.spring.boot.mq.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class Consumer {
     @KafkaListener(topics = "test")
-    public void test(String message) {
-        System.out.println(message);
+    public void consumer(String message) throws InterruptedException {
+        log.info("consumer start...");
+        log.info("message:{}", message);
+        // 睡眠5分钟多1秒，看超时重复消费问题
+        Thread.sleep(1000 * 60 * 5 + 1000);
+        log.info("consumer end.");
     }
 }
