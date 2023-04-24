a453bcadfcd81558db49c5961f55c396e4bf39dc
dunhanson <dunhanson@gmail.com>
2023-04-24 15:40:30 +0800

    feat: 添加authentication&principal

--- gradle-jdk8-spring-security-demo/src/main/java/site/dunhanson/gradle/jdk8/spring/security/demo/controller/LoginController.java
+++ gradle-jdk8-spring-security-demo/src/main/java/site/dunhanson/gradle/jdk8/spring/security/demo/controller/LoginController.java
@@ -1,7 +1,13 @@
 package site.dunhanson.gradle.jdk8.spring.security.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+import java.util.Collection;
 
 /**
  * 登录
@@ -19,5 +25,25 @@ public class LoginController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello spring security";
+    }
+
+    @GetMapping("/user")
+    public void userInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        System.out.println("name = " + name);
+        System.out.println("authorities = " + authorities);
+    }
+
+    @RequestMapping("/authentication")
+    public void authentication(Authentication authentication) {
+        System.out.println("authentication = " + authentication);
+    }
+
+    @RequestMapping("/principal")
+    public void principal(Principal principal) {
+        System.out.println("principal = " + principal);
+
     }
 }
