b9447b66867e469a08dfbe18e15ccee2951aeda7
dunhanson <dunhanson@gmail.com>
2023-04-24 08:53:36 +0800

    feat: gradle-jdk8-spring-security-demo

---
+++ gradle-jdk8-spring-security-demo/src/main/java/site/dunhanson/gradle/jdk8/spring/security/demo/GradleJdk8SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package site.dunhanson.gradle.jdk8.spring.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJdk8SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleJdk8SpringSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ gradle-jdk8-spring-security-demo/src/test/java/site/dunhanson/gradle/jdk8/spring/security/demo/GradleJdk8SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dunhanson.gradle.jdk8.spring.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJdk8SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
