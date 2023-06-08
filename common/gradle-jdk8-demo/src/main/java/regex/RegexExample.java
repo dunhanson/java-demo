7b4b77c40498b772dd6ba5376cfbbef32f5328eb
dunhanson <dunhanson@gmail.com>
2023-06-08 23:17:57 +0800

    feat: slf4j&logback

---
+++ common/gradle-jdk8-demo/src/main/java/regex/RegexExample.java
@@ -0,0 +1,14 @@
+package regex;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class RegexExample {
+    public static void main(String[] args) {
+        String str = "#sql{select date_format(date_add(now(), interval -30 day), '%Y%-%m-%d') from dual}";
+        String pattern = "^#sql\\{\\([\\S|\\s]+\\)\\\\}$";
+        boolean matches = Pattern.matches(pattern, str);
+        log.info("matches:{}", matches);
+    }
+}
