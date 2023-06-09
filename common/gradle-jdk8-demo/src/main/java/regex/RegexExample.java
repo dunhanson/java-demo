b60547cb9ab966f645495c42f92439ea567f2dd2
dunhanson <dunhanson@gmail.com>
2023-06-09 10:05:13 +0800

    feat: RegexExample

--- common/gradle-jdk8-demo/src/main/java/regex/RegexExample.java
+++ common/gradle-jdk8-demo/src/main/java/regex/RegexExample.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class RegexExample {
     public static void main(String[] args) {
         String str = "#sql{select date_format(date_add(now(), interval -30 day), '%Y%-%m-%d') from dual}";
-        String pattern = "^#sql\\{\\([\\S|\\s]+\\)\\\\}$";
+        String pattern = "^#sql\\{([\\S|\\s]+)\\}$";
         boolean matches = Pattern.matches(pattern, str);
         log.info("matches:{}", matches);
     }
